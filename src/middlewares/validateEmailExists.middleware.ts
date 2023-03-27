995f1de763bb8c1222ad568b55d7e5d42eee508c
CarlosX26 <cjunior1@live.com>
2023-03-27 14:38:10 -0300

    fix: middlewares

--- src/middlewares/validateEmailExists.middleware.ts
+++ src/middlewares/validateEmailExists.middleware.ts
@@ -1,25 +1,26 @@
-import { StatusCodes } from "http-status-codes";
-import { Request, Response, NextFunction } from "express";
-import { Client } from "../entities/client.entity";
-import AppDataSource from "../data-source";
+import { StatusCodes } from "http-status-codes"
+import { Request, Response, NextFunction } from "express"
+import { Client } from "../entities/client.entity"
+import AppDataSource from "../data-source"
 
 const validateEmailExistsMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const clientRepo = AppDataSource.getRepository(Client);
-  const clientEmail = await clientRepo.exist({
-    where: { email: req.body.email },
-  });
+  const clientRepo = AppDataSource.getRepository(Client)
+  const clientEmail = await clientRepo
+    .createQueryBuilder("clients")
+    .where("clients.email = :email", { email: req.body.email })
+    .getOne()
 
   if (clientEmail) {
     return res
       .status(StatusCodes.CONFLICT)
-      .json({ message: "Email already exists." });
+      .json({ message: "Email already exists." })
   }
 
-  return next();
-};
+  return next()
+}
 
-export default validateEmailExistsMiddleware;
+export default validateEmailExistsMiddleware
--- src/middlewares/validatePhoneNumberExists.middleware.ts
+++ src/middlewares/validatePhoneNumberExists.middleware.ts
@@ -1,25 +1,28 @@
-import { StatusCodes } from "http-status-codes";
-import { Request, Response, NextFunction } from "express";
-import { Client } from "../entities/client.entity";
-import AppDataSource from "../data-source";
+import { StatusCodes } from "http-status-codes"
+import { Request, Response, NextFunction } from "express"
+import { Client } from "../entities/client.entity"
+import AppDataSource from "../data-source"
 
 const validatePhoneNumberExistsMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const clientRepo = AppDataSource.getRepository(Client);
-  const clientPhoneNumber = await clientRepo.exist({
-    where: { phoneNumber: req.body.phoneNumber },
-  });
+  const clientRepo = AppDataSource.getRepository(Client)
+  const clientPhoneNumber = await clientRepo
+    .createQueryBuilder("clients")
+    .where("clients.phoneNumber = :phoneNumber", {
+      phoneNumber: req.body.phoneNumber,
+    })
+    .getOne()
 
   if (clientPhoneNumber) {
     return res
       .status(StatusCodes.CONFLICT)
-      .json({ message: "PhoneNumber already exists." });
+      .json({ message: "PhoneNumber already exists." })
   }
 
-  return next();
-};
+  return next()
+}
 
-export default validatePhoneNumberExistsMiddleware;
+export default validatePhoneNumberExistsMiddleware
