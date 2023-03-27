ade18d614dab263697c239d893798bcd30800b20
CarlosX26 <cjunior1@live.com>
2023-03-27 08:55:56 -0300

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
-import "express-async-errors";
-import express from "express";
-import handlerError from "./errors/handler";
-import authRouter from "./routers/auth.router";
-import clientRouter from "./routers/clients.router";
-import contactRouter from "./routers/contacts.router";
+import "express-async-errors"
+import express from "express"
+import cors from "cors"
+import handlerError from "./errors/handler"
+import authRouter from "./routers/auth.router"
+import clientRouter from "./routers/clients.router"
+import contactRouter from "./routers/contacts.router"
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/auth", authRouter);
-app.use("/clients", clientRouter);
-app.use("/contacts", contactRouter);
+app.use(cors())
 
-app.use(handlerError);
+app.use("/auth", authRouter)
+app.use("/clients", clientRouter)
+app.use("/contacts", contactRouter)
 
-export default app;
+app.use(handlerError)
+
+export default app
